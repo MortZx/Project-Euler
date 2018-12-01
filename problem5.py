b832031c5aa9cba48b820e7f46f0d826ba250ec9
Mortimer Sotom <mortzxd@gmail.com>
2018-12-01 15:09:39 +0100

    added defensive programming

--- problem5.py
+++ problem5.py
@@ -18,23 +18,31 @@ numbers from 1 to 20?
 
 
 '''
+Iterate through numbers, for each number test divisible for all n specified 
+numbers in an iterated manner unless division has a remainder. 
 
-
-@param 
-@return 
+@param integer - range of numbers that can evenly divide a number
+@return integer - smallest positive number evenly divisible by all n range
 '''
+# Could also use a while True loop and break when x == n.
+# !! Can add a time limit or memory limit to prevent infinite code !!
 def divisibleByAll(n):
+    assert n >= 1, "A positive integer bigger than 0 should be passed!"
     flag = False
     i = 0
-    while flag == False:
-        i += 1
-        for x in range(1, n+1):
-            if i % x != 0:
-                 break 
-            if x == n:
-                flag = True
-    return i
+    try:
+        while flag == False:
+            i += 1
+            for x in range(1, n+1):
+                if i % x != 0:
+                     break 
+                if x == n:
+                    flag = True
+        return i
+    except (TypeError):
+        raise TypeError ("A positive integer bigger than 1 should be passed")
 
 
 if __name__ == "__main__":
-	print(divisibleByAll(10))
+    evenlyDivisibleRange = 20
+    print(divisibleByAll(evenlyDivisibleRange))
