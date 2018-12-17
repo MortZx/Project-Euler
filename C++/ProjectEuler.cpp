159dc89a7dbcb2e812f9fcbdd16c575ed5f7bb66
Mortimer Sotom <mortzxd@gmail.com>
2018-12-17 18:42:30 +0100

    C++ solution to problem 1

---
+++ C++/ProjectEuler.cpp
@@ -0,0 +1,19 @@
+// ProjectEuler.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+# include "Problems.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int lim = 1000;
+	cout << "Solution to problem 1: " << problem1(lim) << endl;
+
+	return 0;
+}
+
