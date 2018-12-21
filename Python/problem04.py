de7cc291f3fe31729ad6c6184a9ff7b696ed885a
Mortimer Sotom <mortzxd@gmail.com>
2018-12-21 20:14:51 +0100

    renamed problems to problem0* to keep index in order

---
+++ Python/problem04.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 19:17:07 2018
+
+@author: MortZ
+
+Project Euler Problem 4
+"""
+
+
+'''
+A palindromic number reads the same both ways. The largest palindrome made from 
+the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+'''
+
+
+'''
+Finds the largest palindrome made from the product of two n-digit specific
+numbers. Iterate through all possible numbers and save all palindroms in a list. 
+!! appending to list is costly !!
+
+@param integer - number of digits for multiplied numbers
+@return integer - largest palindrome of the product of two n digit numbers
+'''
+def maxPalindrom(n):
+    assert n >= 1, "A positive integer bigger than 0 should be passed!"
+    try:
+        minNum = 10**(n-1)
+        maxNum = 10**(n) 
+        palindroms = []
+        for i in range(minNum, maxNum):
+            for x in range (minNum, maxNum):
+                if (str(i*x) == str(i*x)[::-1]):    # [::-1] reverses a string
+                    palindroms.append(i*x)
+        return max(palindroms)
+    except (TypeError):
+        raise TypeError ("A positive integer bigger than 1 should be passed")
+
+def maxPalindrom2(n):
+    assert n >= 1, "A positive integer bigger than 0 should be passed!"
+    try:
+        minNum = 10**(n-1)
+        maxNum = 10**(n) 
+        palindrom = max(i*x
+                        for i in range(minNum, maxNum)
+                        for x in range (minNum, maxNum)
+                        if (str(i*x) == str(i*x)[::-1])
+                        )
+        return palindrom
+    except (TypeError):
+        raise TypeError ("A positive integer bigger than 1 should be passed")
+
+
+if __name__ == "__main__":
+    numDigits = 3
+    print(maxPalindrom2(numDigits))
---
+++ Python/problem07.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 17:07:14 2018
+
+@author: MortZ
+
+Project Euler problem 7
+"""
+
+
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
+we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+'''
+
+
+'''
+Incrementally check if the current number is evenly divisible by the current 
+list of prime numbers. If not, add to the list of prime numbers until the nth
+specified prime number is found
+
+@param integer - nth prime number to find
+@return integer - nth specified prime number
+'''
+def findPrimeByIndex(n):
+    assert n >= 1, "A positive intger should be passed!"
+    primeList = [2]
+    i = 1
+    while len(primeList) < n:
+        i += 1
+        # iterate through primelist
+        # check if divisible by all prime, if yes +1, 
+        for prime in primeList:
+            if i % prime == 0:
+                break
+            else:
+                if prime == primeList[-1]:
+                    primeList.append(i)
+    
+    return primeList[n-1]
+    
+    
+
+if __name__ == "__main__":
+    index = 10001
+    print(findPrimeByIndex(index))
