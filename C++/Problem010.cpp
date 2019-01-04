20f1ca5f67d9f002adfd1f9c597f653099e56387
Mortimer Sotom <mortzxd@gmail.com>
2019-01-04 19:02:40 +0100

    efficient solution by calculating sum in same function as finding primes and not having to return vector --> possible better solution of calculating primes without keeping a list of primes?

--- C++/Problem010.cpp
+++ C++/Problem010.cpp
@@ -15,14 +15,16 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 
-
 /*
 Problem 7:Find prime by index
---> Edit to return the list of primes in a range
+--> Edit to sum prime numbers
 */
-std::vector<unsigned int> findPrimeBelow(unsigned int n) {
+unsigned long long int problem10(unsigned int n) {
+	// first prime number is 2
 	std::vector<unsigned int> primeList{ 2 };
+	unsigned long long int sum = 2; 
 
+	// iterate through all number to find the first n prime numbers and keeping track of the sum
 	for (unsigned int i = 2; i < n; ++i) {
 		for (auto prime : primeList) {
 			if (i % prime == 0) {
@@ -31,19 +33,11 @@ std::vector<unsigned int> findPrimeBelow(unsigned int n) {
 			else {
 				if (prime == primeList.back()) {
 					primeList.push_back(i);
+					sum += i;
 				}
 			}
 
 		}
-	}
-	return primeList;
-}
-
-
-unsigned long long int problem10(unsigned int n) {
-	unsigned long long int sum = 0;
-	for (auto& num : findPrimeBelow(n)) {
-		sum += n;
 	}
 	return sum;
 }
