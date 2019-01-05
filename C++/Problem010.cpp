8d4c29616c98ca1e6b28bc050a4ab2ace506df69
Mortimer Sotom <mortzxd@gmail.com>
2019-01-05 19:41:06 +0100

    Fixed typos in documentation

--- C++/Problem010.cpp
+++ C++/Problem010.cpp
@@ -6,12 +6,9 @@
 
 /*
 =================
-=   Problem 9   =	Summation of primes
+=   Problem 10   =	Summation of primes
 ==================
-
-
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
 Find the sum of all the primes below two million.
 */
 
@@ -22,7 +19,7 @@ Problem 7:Find prime by index
 unsigned long long int problem10(unsigned int n) {
 	// first prime number is 2
 	std::vector<unsigned int> primeList{ 2 };
-	unsigned long long int sum = 2; 
+	unsigned long long int sum = 2;
 
 	// iterate through all number to find the first n prime numbers and keeping track of the sum
 	for (unsigned int i = 2; i < n; ++i) {
