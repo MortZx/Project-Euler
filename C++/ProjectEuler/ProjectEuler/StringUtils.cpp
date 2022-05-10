a6f776d1a2cc2e76b17e9a01d63096592f2c0046
MortZx <mortzxd@gmail.com>
2022-05-10 23:20:02 +0100

    StringUtils StringAddition extracted from problem 13

--- C++/ProjectEuler/ProjectEuler/StringUtils.cpp
+++ C++/ProjectEuler/ProjectEuler/StringUtils.cpp
@@ -12,7 +12,65 @@ Useful functions compiled from problem solutions.
 
 namespace StringUtils
 {
-	// Refer to problem 13 for string addition
+	void StringAddition(const std::string str1, const std::string str2, std::string& res)
+	{
+		res.clear();
+		size_t minLen = str1.length() - 1;
+		if (str1.length() > str2.length())
+		{
+			minLen = str2.length() - 1;
+		}
+
+		// Carry out addition arithmetic for each digit 
+		int carry = 0;
+		size_t i = 0;
+		while (i <= minLen)
+		{
+			int d1 = (str1.at(str1.length() - i - 1) - '0');
+			int d2 = (str2.at(str2.length() - i - 1) - '0');
+
+			int dRes = (str1.at(str1.length() - i - 1) - '0') + (str2.at(str2.length() - i - 1) - '0') + carry;
+			carry = dRes / 10;
+			res.insert(0, std::to_string(dRes % 10));
+
+			i++;
+		}
+
+		// Continue until we no longer have a carry
+		size_t j = 0;
+		while (carry)
+		{
+			int dRes = 0;
+			if (str1.length() > str2.length() && str1.length() > i + j)
+			{
+				dRes = (str1.at(str1.length() - 1 - i - j) - '0') + carry;
+			}
+			else if (str1.length() < str2.length() && str2.length() > i + j)
+			{
+				dRes = (str2.at(str2.length() - 1 - i - j) - '0') + carry;
+			}
+			else
+			{
+				dRes = carry;
+			}
+
+			carry = dRes / 10;
+			res = std::to_string((dRes % 10)) + res;
+			j++;
+		}
+
+		// Add the front digits of the longest number
+		if (str1.length() > str2.length() && str1.length() > i + j)
+		{
+			res = str1.substr(0, str1.length() - i - j) + res;
+		}
+		else if (str1.length() < str2.length() && str2.length() > i + j)
+		{
+			res = str2.substr(0, str2.length() - i - j) + res;
+		}
+	}
+
+
 	void StringMultiplication(const std::string strMultiplicand, const std::string strMultiplier, std::string& result)
 	{
 		if (strMultiplicand.empty() || strMultiplier.empty())
--- C++/ProjectEuler/ProjectEuler/StringUtils.h
+++ C++/ProjectEuler/ProjectEuler/StringUtils.h
@@ -5,10 +5,20 @@
 namespace StringUtils
 {
 	/**
-	 * Checks if a number is fully divisible by another
+	 * Addition with strings for long numbers
 	 *
 	 * @param strMultiplicand - String number 1
 	 * @param strMultiplier - String number 2
+	 * @param res - String to store result
+	 */
+	void StringAddition(const std::string str1, const std::string str2, std::string& res);
+
+	/**
+	 * Multiplication with strings for long numbers
+	 *
+	 * @param strMultiplicand - String number 1
+	 * @param strMultiplier - String number 2
+	 * @param res - String to store result
 	 */
 	void StringMultiplication(const std::string strMultiplicand, const std::string strMultiplier, std::string& result);
 }
